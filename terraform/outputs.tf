3d6e4bf72994e8c24b446c212162d9d990188814
kanhaiyak23 <kanhaiyakumarsingh086@gmail.com>
2026-04-20 12:38:16 +0530

    infra: add terraform ECS deployment (ECR, cluster, task, service)

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "ecr_repository_url" {
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.app_repo.repository_url
+}
+
+output "ecs_cluster_name" {
+  description = "ECS cluster name"
+  value       = aws_ecs_cluster.app_cluster.name
+}
+
+output "ecs_service_name" {
+  description = "ECS service name"
+  value       = aws_ecs_service.app_service.name
+}
+
+output "task_definition_arn" {
+  description = "ECS task definition ARN"
+  value       = aws_ecs_task_definition.app_task.arn
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "app_name" {
+  description = "Application name used for all resource names"
+  type        = string
+  default     = "devops-static-app"
+}
+
+variable "image_uri" {
+  description = "Full ECR image URI to deploy (e.g. 123456789.dkr.ecr.us-east-1.amazonaws.com/app:sha)"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "VPC Subnet ID for ECS task network (passed via TF_VAR_subnet_id secret)"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security Group ID for ECS task (passed via TF_VAR_security_group_id secret)"
+  type        = string
+}
